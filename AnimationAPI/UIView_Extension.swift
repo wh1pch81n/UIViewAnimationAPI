b03ad5b3e068fa846f1d80f5441daefb3420502e
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2016-12-10 17:12:40 -0500

    class based Animator with a lot less code

--- AnimationAPI/UIView_Extension.swift
+++ AnimationAPI/UIView_Extension.swift
@@ -8,153 +8,49 @@
 
 import UIKit
 
-// MARK: - Animator properties
-public protocol AnimatorComposition_Properties {
-	var _duration: TimeInterval { get set }
-	var _animations: () -> () { get set }
-}
-
-public protocol AnimatorCompositionWithDelay_Properties {
-	var _delay: TimeInterval { get set }
-}
-
-public protocol AnimatorCompositionWithOptions_Properties: AnimatorComposition_Properties, AnimatorCompositionWithDelay_Properties {
-	var _options: UIViewAnimationOptions { get set }
-}
-
-public protocol AnimatorCompositionWithSpringVelocity_Properties: AnimatorCompositionWithOptions_Properties {
-	var _springDamping: CGFloat { get set }
-	var _initialSpringVelocity: CGFloat { get set }
-	
-}
-
-// MARK: - Animator methods
-
-public protocol AnimatorComposition {
-	func duration(_ duration: TimeInterval) -> Self
-	func animations(_ animations: @escaping () -> ()) -> Self
-	
-	func completion(_ completion: @escaping (Bool) -> ())
-	
-	// transition
-	func delay(_ delay: TimeInterval) -> AnimatorCompositionWithOptions
-	func options(_ options: UIViewAnimationOptions) -> AnimatorCompositionWithOptions
-	func usingSpringWithDamping(_ springWithDamping: CGFloat, initialSpringVelocity: CGFloat) -> AnimatorCompositionWithSpringVelocity
-}
-
-public protocol AnimatorCompositionWithDelay {
-	func delay(_ delay: TimeInterval) -> Self
-}
-
-public protocol AnimatorCompositionWithOptions: AnimatorComposition, AnimatorCompositionWithDelay {
-	func options(_ options: UIViewAnimationOptions) -> Self
-}
-
-public protocol AnimatorCompositionWithSpringVelocity: AnimatorCompositionWithOptions {
-	func usingSpringWithDamping(_ springWithDamping: CGFloat, initialSpringVelocity: CGFloat) -> Self
-}
-
-// MARK: - Protocol Extensions
-
-extension AnimatorComposition where Self: AnimatorComposition_Properties {
-	public func duration(_ duration: TimeInterval) -> Self {
-		var a = self
-		a._duration = duration
-		return a
-	}
-	public func animations(_ animations: @escaping () -> ()) -> Self {
-		var a = self
-		a._animations = animations
-		return a
-	}
-	public func completion(_ completion: @escaping (Bool) -> ()) {
-		UIView.animate(withDuration: _duration
-			, animations: _animations
-			, completion: completion)
-	}
-	
-	// transition
-	public func delay(_ delay: TimeInterval) -> AnimatorCompositionWithOptions {
-		return (self as! AnimatorCompositionWithOptions).delay(delay)
-	}
-	public func options(_ options: UIViewAnimationOptions) -> AnimatorCompositionWithOptions {
-		return (self as! AnimatorCompositionWithOptions).options(options)
-	}
-	public func usingSpringWithDamping(_ springWithDamping: CGFloat, initialSpringVelocity: CGFloat) -> AnimatorCompositionWithSpringVelocity {
-		return (self as! AnimatorCompositionWithSpringVelocity)
-			.usingSpringWithDamping(springWithDamping
-				, initialSpringVelocity: initialSpringVelocity)
-	}
-}
-
-extension AnimatorCompositionWithDelay where Self: AnimatorCompositionWithDelay_Properties {
-	public func delay(_ delay: TimeInterval) -> Self {
-		var a = self
-		a._delay = delay
-		return a
-	}
-}
-
-extension AnimatorCompositionWithOptions where Self: AnimatorCompositionWithOptions_Properties {
-
-	public func options(_ options: UIViewAnimationOptions) -> Self {
-		var a = self
-		a._options = options
-		return a
-	}
-	public func completion(_ completion: @escaping (Bool) -> ()) {
-		UIView.animate(withDuration: _duration
-			, delay: _delay
-			, options: _options
-			, animations: _animations
-			, completion: completion)
-	}
-}
-
-extension AnimatorCompositionWithSpringVelocity where Self: AnimatorCompositionWithSpringVelocity_Properties {
-	
-	func usingSpringWithDamping(_ springWithDamping: CGFloat, initialSpringVelocity: CGFloat) -> Self {
-		var a = self
-		a._springDamping = springWithDamping
-		a._initialSpringVelocity = initialSpringVelocity
-		return a
-	}
-	
-	public func completion(_ completion: @escaping (Bool) -> ()) {
-		UIView.animate(
-			withDuration: _duration
-			, delay: _delay
-			, usingSpringWithDamping: _springDamping
-			, initialSpringVelocity: _initialSpringVelocity
-			, options: _options
-			, animations: _animations
-			, completion: completion)
-	}
-
-}
-
-
 // MARK: - UIKit Extension
 extension UIKit.UIView {
-	
-	fileprivate struct Animator:
-		AnimatorComposition, AnimatorComposition_Properties
-		, AnimatorCompositionWithOptions, AnimatorCompositionWithOptions_Properties
-		, AnimatorCompositionWithSpringVelocity, AnimatorCompositionWithSpringVelocity_Properties
-	{
 
-		var _duration: TimeInterval = 0
-		var _animations: () -> () = {}
+	public class Animator {
+		fileprivate var animatorBlock: ((@escaping () -> (), @escaping (Bool) -> ()) -> ())
+		fileprivate var animations: () -> () = { _ in }
 		
-		var _delay: TimeInterval = 0
-		var _options: UIViewAnimationOptions = .curveEaseInOut
+		public init(animatorBlock: @escaping ((@escaping () -> (), @escaping (Bool) -> ()) -> ())) {
+			self.animatorBlock = animatorBlock
+		}
 		
-		var _springDamping: CGFloat = 1
-		var _initialSpringVelocity: CGFloat = 0
+		public func animations(_ animations: @escaping () -> ()) -> Self {
+			self.animations = animations
+			return self
+		}
+		
+		public func completion(_ completion: @escaping (Bool) -> ()) {
+			animatorBlock(animations, completion)
+		}
 	}
 
-	public static func animate(withDuration duration: TimeInterval) -> AnimatorComposition {
-		return Animator().duration(duration)
+	public static func animate(withDuration duration: TimeInterval) -> Animator {
+		return Animator(animatorBlock: {
+			UIView.animate(withDuration: duration, animations: $0, completion: $1)
+		})
+	}
+	
+	public static func animate(withDuration duration: TimeInterval, delay: TimeInterval, options: UIViewAnimationOptions) -> Animator {
+		return Animator(animatorBlock: {
+			UIView.animate(withDuration: duration, delay: delay, options: options, animations: $0, completion: $1)
+		})
+	}
+	
+	public static func animate(withDuration duration: TimeInterval, delay: TimeInterval, usingSpringWithDamping: CGFloat, initialSpringVelocity: CGFloat, options: UIViewAnimationOptions ) -> Animator {
+		return Animator(animatorBlock: {
+			UIView.animate(withDuration: duration, delay: delay, usingSpringWithDamping: usingSpringWithDamping, initialSpringVelocity: initialSpringVelocity, options: options, animations: $0, completion: $1)
+		})
+	}
+	
+	public static func animateKeyframes(withDuration duration: TimeInterval, delay: TimeInterval, options: UIViewKeyframeAnimationOptions) -> Animator {
+		return Animator(animatorBlock: {
+			UIView.animateKeyframes(withDuration: duration, delay: delay, options: options, animations: $0, completion: $1)
+		})
 	}
 	
 }
--- AnimationAPI/ViewController.swift
+++ AnimationAPI/ViewController.swift
@@ -14,65 +14,55 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
+		let v = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+		v.backgroundColor = .blue
+		self.view.addSubview(v)
+		
+		UIView.animateKeyframes(withDuration: 5
+			, delay: 0
+			, options: UIViewKeyframeAnimationOptions.calculationModeLinear)
+			.animations {
+				UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2) {
+					v.frame.origin = CGPoint(x: 20, y: 0)
+				}
+				UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.2) {
+					v.frame.origin = CGPoint(x: 20, y: 20)
+				}
+				UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.2) {
+					v.frame.origin = CGPoint(x: 0, y: 20)
+				}
+				UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.4) {
+					v.frame.origin = CGPoint(x: 0, y: 0)
+				}
+			}
+			.completion {_ in}
+		
+		
+		
 		UIView.animate(withDuration: 5)
 			.animations {
 				self.view.backgroundColor = .red
 			}
 			.completion { _ in
 				self.view.backgroundColor = .white
-				
-				UIView.animate(withDuration: 5)
-					.delay(5)
+				UIView.animate(withDuration: 5, delay: 1, options: UIViewAnimationOptions.curveEaseInOut)
 					.animations {
 						self.view.backgroundColor = .green
 					}.completion { _ in
 						self.view.backgroundColor = .white
 						
 						let v = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-						v.backgroundColor = .blue
+						v.backgroundColor = .purple
 						self.view.addSubview(v)
-						UIView.animate(withDuration: 5)
-							.delay(1)
-							.usingSpringWithDamping(0.1, initialSpringVelocity: 40)
+						UIView.animate(withDuration: 5, delay: 1, usingSpringWithDamping: 0.1, initialSpringVelocity: 40, options: UIViewAnimationOptions.curveEaseInOut)
 							.animations {
 								v.frame.origin.x = 150
 							}.completion({ _ in
 								v.frame.origin.x = 0
-								
-								UIView.animateKeyframes(withDuration: 5
-									, delay: 0
-									, options: UIViewKeyframeAnimationOptions.calculationModeLinear
-									, animations: {
-										UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: 0.2, animations: {
-											v.frame.origin = CGPoint(x: 20, y: 0)
-										})
-										UIView.addKeyframe(withRelativeStartTime: 0.2, relativeDuration: 0.2, animations: {
-											v.frame.origin = CGPoint(x: 20, y: 20)
-										})
-										UIView.addKeyframe(withRelativeStartTime: 0.4, relativeDuration: 0.2, animations: {
-											v.frame.origin = CGPoint(x: 0, y: 20)
-										})
-										UIView.addKeyframe(withRelativeStartTime: 0.6, relativeDuration: 0.4, animations: {
-											v.frame.origin = CGPoint(x: 0, y: 0)
-										})
-								}, completion: { _ in
-									
-								})
 							})
 				}
 		}
 		
-		
-		
-		
-
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
