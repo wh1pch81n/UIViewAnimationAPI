80e25f1955a555513fc3e9f416e1b5aab9f8cd0f
Derrick Ho <derrickho@derricks-mbp.fios-router.home>
2016-12-10 11:15:51 -0500

    Initial Commit

---
+++ AnimationAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationAPI
+//
+//  Created by Derrick Ho on 12/10/16.
+//  Copyright © 2016 Derrick Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
